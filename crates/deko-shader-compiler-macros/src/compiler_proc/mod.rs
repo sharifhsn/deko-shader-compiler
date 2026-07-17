4148701348d503929afd1c927391a57e73d496ab
sharif <sharif@monark-markets.com>
2026-07-16 23:37:33 -0500

    feat: port NAK IR proc macros

---
+++ crates/deko-shader-compiler-macros/src/compiler_proc/mod.rs
@@ -0,0 +1,9 @@
+// Copyright © 2024 Collabora, Ltd.
+// SPDX-License-Identifier: MIT
+
+#[rustfmt::skip]
+pub(crate) mod as_slice;
+#[rustfmt::skip]
+pub(crate) mod enum_as_u8;
+#[rustfmt::skip]
+pub(crate) mod from_variants;
